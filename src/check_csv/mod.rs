ac974b82b35d731a4a2623aa3e56a1d43beda9a8
Outvi V <git@outv.im>
2021-07-04 08:51:44 +0800

    feat: .collect() deserialized results

--- src/check_csv/mod.rs
+++ src/check_csv/mod.rs
@@ -1,23 +1,18 @@
 pub mod types;
 
 use crate::Music;
-use csv::Reader;
+use csv::{Reader, Error};
 use std::io::Read;
 
 pub fn check_csv(source: impl Read) -> Result<Vec<Music>, String> {
     let mut reader = Reader::from_reader(source);
-    let mut ret = vec![];
-
-    for result in reader.deserialize() {
-        match result {
-            Ok(v) => {
-                ret.push(v);
-            }
-            Err(e) => {
-                return Err(e.to_string());
-            }
+    match reader.deserialize().collect::<Result<Vec<Music>, Error>>() {
+        Ok(data) => {
+            Ok(data)
+        }
+        Err(err) => {
+            Err(err.to_string())
         }
     }
 
-    Ok(ret)
 }
