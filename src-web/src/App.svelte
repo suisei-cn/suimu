93173eeccdb3696bab18b5d286daf413a30bb2b9
Outvi V <git@outv.im>
2021-08-04 00:06:52 +0800

    feat: display MaybeMusic in table

--- src-web/src/App.svelte
+++ src-web/src/App.svelte
@@ -4,7 +4,14 @@
   import { dialog } from '@tauri-apps/api'
   import { get_maybemusic_by_csv_path } from './invoke'
 
+  import dayjs from 'dayjs'
+  import { Grid } from 'gridjs'
+  import { onMount } from 'svelte'
+
   let csvMaybeMusic: MaybeMusic[] = []
+  let grid: Grid = undefined
+
+  let musicTable
 
   function selectCsvFile() {
     dialog
@@ -15,11 +22,62 @@
       .then((x) => {
         if (x.ok) {
           csvMaybeMusic = x.object
+          updateTable(x.object)
         } else {
           console.error(x.message)
         }
       })
   }
+
+  const isNope = (v: any) => v === undefined || v === null
+
+  function toClipRange(fr: number | null, to: number | null): string {
+    if (isNope(fr) && isNope(to)) {
+      return '(full)'
+    }
+    if (isNope(fr)) {
+      return `(begin) - ${to.toFixed(2)}`
+    }
+    if (isNope(to)) {
+      return `${fr.toFixed(2)} - (end)`
+    }
+    return `${fr.toFixed(2)} - ${to.toFixed(2)}`
+  }
+
+  function updateTable(data: MaybeMusic[]) {
+    grid
+      .updateConfig({
+        data: data.map((r) => [
+          dayjs(r.datetime).format('YYYY-MM-DD HH:mm'),
+          r.video_type,
+          r.video_id,
+          toClipRange(r.clip_start, r.clip_end),
+          r.title,
+          r.artist,
+          r.performer,
+          r.comment,
+        ]),
+      })
+      .forceRender()
+  }
+
+  onMount(() => {
+    grid = new Grid({
+      columns: [
+        'Date',
+        'Video Type',
+        'Video ID',
+        'Range',
+        'Title',
+        'Artist',
+        'Performer',
+        'Comment',
+      ],
+      data: [],
+      fixedHeader: true,
+      height: '50vh',
+    }).render(musicTable)
+  })
 </script>
 
 <main>
@@ -32,6 +90,7 @@
         {csvMaybeMusic.length} clips found.
       </div>
     {/if}
+    <div id="musicTableDiv" bind:this="{musicTable}"></div>
   </div>
 </main>
 
@@ -48,6 +107,10 @@
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+  }
+
+  #musicTableDiv {
+    margin-top: 15px;
   }
 
   @media (min-width: 640px) {
--- src-web/src/main.ts
+++ src-web/src/main.ts
@@ -1,4 +1,5 @@
 import App from './App.svelte'
+import 'gridjs/dist/theme/mermaid.css'
 
 const app = new App({
   target: document.body,
